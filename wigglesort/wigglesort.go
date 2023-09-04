7b9cc389c321813db82b35112cb0424a8370cc0e
nolan-is-coding <110746237+nolan-is-coding@users.noreply.github.com>
2023-09-04 04:28:00 +0000

    fix package

--- wigglesort/wigglesort.go
+++ wigglesort/wigglesort.go
@@ -5,9 +5,14 @@ import (
 	"sort"
 )
 
+// from list of integer
+// create a wave sort  up down up down up ...
+// sorted
+//
+//	take the first and then take last elem keep until array is empty.
 func WiggleSort(arr []int) []int {
 	sort.Ints(arr)
-	fmt.Println("arr", arr)
+	fmt.Println(arr)
 	res := []int{}
 	for len(arr) > 0 {
 		if len(arr) > 0 {
@@ -19,9 +24,11 @@ func WiggleSort(arr []int) []int {
 			arr = arr[:len(arr)-1]
 		}
 	}
-	fmt.Println(res)
 	return res
 }
+
+// Time Complexity : n log n
+// Space: lenght of array
 
 func main() {
 	fmt.Println("not doing anything")
